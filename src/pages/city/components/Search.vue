0e9bb37320213b3559f6a6b516311100e1264fe6
yotcap <nx0163@163.com>
2018-08-21 17:28:04 +0800

    添加城市页面的搜索框

---
+++ src/pages/city/components/Search.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class="search">
+        <input class="s-input" placeholder="输入城市名或拼音" />
+    </div>
+</template>
+<script>
+export default {
+    data () {
+        return {
+        }
+    }
+}
+</script>
+<style scoped lang="stylus">
+@import '~styles/varibles.styl'
+
+.search
+    height: .72rem
+    padding: 0 .1rem
+    background: $themeColor
+    .s-input
+        width: 100%
+        height: .62rem
+        line-height: .62rem
+        padding: 0 .1rem
+        border-radius: .06rem
+        text-align: center
+        box-sizing: border-box
+        color: #666
+</style>
