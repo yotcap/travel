cd62d714aa2d174f4da34df4bcf985268e21ad60
yotcap <nx0163@163.com>
2018-08-23 16:28:34 +0800

    完成城市搜索页的布局

---
+++ src/pages/city/components/Alphabet.vue
@@ -0,0 +1,37 @@
+<template>
+    <div>
+        <ul class="list">
+            <li class="item">A</li>
+            <li class="item">A</li>
+            <li class="item">A</li>
+            <li class="item">A</li>
+            <li class="item">A</li>
+        </ul>
+    </div>
+</template>
+<script>
+export default {
+    name: 'alphabet',
+    data () {
+        return {
+        }
+    }
+}
+</script>
+<style scoped lang="stylus">
+@import '~styles/varibles.styl'
+
+.list
+    width: .4rem
+    display: flex
+    flex-direction: column
+    justify-content: center
+    position: absolute
+    top: 1.58rem
+    right: 0
+    bottom: 0
+    .item
+        line-height: .4rem
+        text-align: center
+        color: $themeColor
+</style>
