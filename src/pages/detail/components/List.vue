b6a51a3c1ce341f854a7c98f1b471c67ab987958
yotcap <nx0163@163.com>
2018-08-27 15:37:04 +0800

    新增详情页面中的 list 组件，并实现递归调用

---
+++ src/pages/detail/components/List.vue
@@ -0,0 +1,46 @@
+<template>
+  <div>
+    <div v-for="(item, index) of list" :key="index">
+      <div class="item-title border-bottom">
+        <span class="item-title-icon"></span>
+        {{ item.title }}
+      </div>
+      <div v-if="item.children" class="item-children">
+        <detail-list :list="item.children"></detail-list>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  name: 'DetailList',
+  data () {
+    return {
+    }
+  },
+  props: {
+    list: {
+      type: Array,
+      default: []
+    }
+  }
+}
+</script>
+<style scoped lang="stylus">
+.item-title
+  line-height: .8rem
+  font-size: .32rem
+  padding: 0 .2rem
+.item-title-icon
+  width: .36rem
+  height: .36rem
+  margin-right: .1rem
+  display: inline-block
+  position: relative
+  top: .06rem
+  left: .06rem
+  background: url(http://s.qunarzz.com/piao/image/touch/sight/detail.png) 0 -.9rem no-repeat
+  background-size: .4rem 3rem
+.item-children
+  padding: 0 .2rem
+</style>
